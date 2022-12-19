1cbdf979df8121aef8f0e06d422f21e07c0e26ff
Dan Nguyen <pedantic@gmail.com>
2022-12-19 01:49:17 -0600

    day19: Add initial solution

---
+++ src/day19.rs
@@ -0,0 +1,253 @@
+use std::collections::BinaryHeap;
+
+use ahash::HashSet;
+use aoc_runner_derive::{aoc, aoc_generator};
+use nom::{bytes::complete::tag, combinator::map, sequence::tuple, IResult};
+use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct BluePrint {
+    num: i64,
+    ore: i64,
+    clay: i64,
+    obsidian: (i64, i64),
+    geode: (i64, i64),
+}
+
+fn num(s: &str) -> IResult<&str, i64> {
+    nom::character::complete::i64(s)
+}
+
+fn parse_blueprint(s: &str) -> IResult<&str, BluePrint> {
+    map(
+        tuple((
+            tag("Blueprint "),
+            num,
+            tag(": Each ore robot costs "),
+            num,
+            tag(" ore. Each clay robot costs "),
+            num,
+            tag(" ore. Each obsidian robot costs "),
+            num,
+            tag(" ore and "),
+            num,
+            tag(" clay. Each geode robot costs "),
+            num,
+            tag(" ore and "),
+            num,
+            tag(" obsidian."),
+        )),
+        |x| BluePrint {
+            num: x.1,
+            ore: x.3,
+            clay: x.5,
+            obsidian: (x.7, x.9),
+            geode: (x.11, x.13),
+        },
+    )(s)
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord, Hash)]
+struct Counts {
+    geode: i64,
+    geode_robot: i64,
+    obsidian_robot: i64,
+    clay_robot: i64,
+    ore_robot: i64,
+    ore: i64,
+    clay: i64,
+    obsidian: i64,
+}
+
+impl Default for Counts {
+    fn default() -> Self {
+        Self {
+            ore_robot: 1,
+            ore: Default::default(),
+            clay_robot: Default::default(),
+            clay: Default::default(),
+            obsidian_robot: Default::default(),
+            obsidian: Default::default(),
+            geode_robot: Default::default(),
+            geode: Default::default(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
+struct Priority<T: Ord>(i64, T);
+
+fn simulate<const PART: usize>(bp: &BluePrint, total_minutes: i64) -> i64 {
+    let mut seen = HashSet::default();
+    let mut queue = BinaryHeap::new();
+
+    queue.push(Priority(0, (0, Counts::default())));
+
+    while let Some(Priority(_, (minutes, count))) = queue.pop() {
+        if minutes == total_minutes {
+            return if PART == 1 {
+                count.geode * bp.num
+            } else {
+                count.geode
+            };
+        }
+        if !seen.insert(count) {
+            continue;
+        }
+
+        let time = total_minutes - minutes;
+        let ore = count.ore + count.ore_robot;
+        let clay = count.clay + count.clay_robot;
+        let obsidian = count.obsidian + count.obsidian_robot;
+        let geode = count.geode + count.geode_robot;
+        let minutes = minutes + 1;
+
+        if count.ore >= bp.ore {
+            queue.push(Priority(
+                geode + (count.geode_robot + time - 2) * (time - 2),
+                (
+                    minutes,
+                    Counts {
+                        geode,
+                        ore_robot: count.ore_robot + 1,
+                        ore: ore - bp.ore,
+                        clay,
+                        obsidian,
+                        ..count
+                    },
+                ),
+            ));
+        }
+
+        if count.ore >= bp.clay {
+            queue.push(Priority(
+                geode + (count.geode_robot + time - 2) * (time - 2),
+                (
+                    minutes,
+                    Counts {
+                        geode,
+                        ore: ore - bp.clay,
+                        clay_robot: count.clay_robot + 1,
+                        clay,
+                        obsidian,
+                        ..count
+                    },
+                ),
+            ));
+        }
+
+        if count.ore >= bp.obsidian.0 && count.clay >= bp.obsidian.1 {
+            queue.push(Priority(
+                geode + (count.geode_robot + time - 2) * (time - 2),
+                (
+                    minutes,
+                    Counts {
+                        geode,
+                        obsidian_robot: count.obsidian_robot + 1,
+                        ore: ore - bp.obsidian.0,
+                        clay: clay - bp.obsidian.1,
+                        obsidian,
+                        ..count
+                    },
+                ),
+            ));
+        }
+
+        if count.ore >= bp.geode.0 && count.obsidian >= bp.geode.1 {
+            queue.push(Priority(
+                geode + 1 + (count.geode_robot + 1 + time - 2) * (time - 2),
+                (
+                    minutes,
+                    Counts {
+                        geode,
+                        geode_robot: count.geode_robot + 1,
+                        ore: ore - bp.geode.0,
+                        clay,
+                        obsidian: obsidian - bp.geode.1,
+                        ..count
+                    },
+                ),
+            ));
+        } else {
+            // Do nothing
+            queue.push(Priority(
+                geode + (count.geode_robot + time - 2) * (time - 2),
+                (
+                    minutes,
+                    Counts {
+                        geode,
+                        ore,
+                        clay,
+                        obsidian,
+                        ..count
+                    },
+                ),
+            ));
+        }
+    }
+
+    unimplemented!()
+}
+
+#[aoc_generator(day19)]
+pub fn generator(input: &str) -> Vec<BluePrint> {
+    input
+        .lines()
+        .map(|line| parse_blueprint(line).unwrap().1)
+        .collect()
+}
+
+#[aoc(day19, part1)]
+pub fn part1(inputs: &[BluePrint]) -> i64 {
+    inputs.par_iter().map(|bp| simulate::<1>(bp, 24)).sum()
+}
+
+#[aoc(day19, part2)]
+pub fn part2(inputs: &[BluePrint]) -> i64 {
+    inputs
+        .par_iter()
+        .take(3)
+        .map(|bp| simulate::<2>(bp, 32))
+        .product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const SAMPLE: &str = r"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
+
+    #[test]
+    pub fn test_input() {
+        println!("{:?}", generator(SAMPLE));
+
+        // assert_eq!(generator(SAMPLE), Object());
+    }
+
+    #[test]
+    pub fn test1() {
+        // assert_eq!(part1(&generator(SAMPLE)), 33);
+    }
+
+    #[test]
+    pub fn test2() {
+        // assert_eq!(part2(&generator(SAMPLE)), 62);
+    }
+
+    mod regression {
+        use super::*;
+
+        const INPUT: &str = include_str!("../input/2022/day19.txt");
+        const ANSWERS: (i64, i64) = (1264, 13475);
+
+        #[test]
+        pub fn test() {
+            let input = INPUT.trim_end_matches('\n');
+            let output = generator(input);
+
+            // assert_eq!(part1(&output), ANSWERS.0);
+            // assert_eq!(part2(&output), ANSWERS.1);
+        }
+    }
+}
