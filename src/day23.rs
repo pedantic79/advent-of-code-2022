4f427a5d24ba38b8dca0b86b6208d494d98ed321
Dan Nguyen <pedantic@gmail.com>
2022-12-23 00:29:44 -0600

    day23: Refactor common solve fn

--- src/day23.rs
+++ src/day23.rs
@@ -1,8 +1,8 @@
-use ahash::{HashMap, HashSet};
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
+use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
 
-const ALL: [(i64, i64); 8] = [
+const ALL: [(i32, i32); 8] = [
     (-1, -1),
     (-1, 0),
     (-1, 1),
@@ -12,13 +12,13 @@ const ALL: [(i64, i64); 8] = [
     (1, 0),
     (1, 1),
 ];
-const NORTH: [(i64, i64); 3] = [(-1, -1), (-1, 0), (-1, 1)];
-const SOUTH: [(i64, i64); 3] = [(1, -1), (1, 0), (1, 1)];
-const WEST: [(i64, i64); 3] = [(-1, -1), (0, -1), (1, -1)];
-const EAST: [(i64, i64); 3] = [(-1, 1), (0, 1), (1, 1)];
+const NORTH: [(i32, i32); 3] = [(-1, -1), (-1, 0), (-1, 1)];
+const SOUTH: [(i32, i32); 3] = [(1, -1), (1, 0), (1, 1)];
+const WEST: [(i32, i32); 3] = [(-1, -1), (0, -1), (1, -1)];
+const EAST: [(i32, i32); 3] = [(-1, 1), (0, 1), (1, 1)];
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-pub struct Elf(i64, i64);
+pub struct Elf(i32, i32);
 
 impl Elf {
     fn tick(&self, map: &HashSet<Elf>, i: usize) -> Option<Elf> {
@@ -48,7 +48,7 @@ impl Elf {
         })
     }
 
-    fn check(&self, map: &HashSet<Elf>, dirs: &[(i64, i64)]) -> bool {
+    fn check(&self, map: &HashSet<Elf>, dirs: &[(i32, i32)]) -> bool {
         dirs.iter()
             .map(|(y, x)| Elf(self.0 + y, self.1 + x))
             .all(|elf| !map.contains(&elf))
@@ -63,7 +63,7 @@ pub fn generator(input: &str) -> HashSet<Elf> {
         .flat_map(|(r_idx, row)| {
             row.bytes().enumerate().filter_map(move |(c_idx, cell)| {
                 if cell == b'#' {
-                    Some(Elf(r_idx as i64, c_idx as i64))
+                    Some(Elf(r_idx as i32, c_idx as i32))
                 } else {
                     None
                 }
@@ -72,61 +72,55 @@ pub fn generator(input: &str) -> HashSet<Elf> {
         .collect()
 }
 
-#[aoc(day23, part1)]
-pub fn part1(inputs: &HashSet<Elf>) -> u64 {
+fn solve<const ROUNDS: usize, N>(
+    inputs: &HashSet<Elf>,
+    ret: impl Fn(usize, &HashSet<Elf>) -> N,
+) -> N {
     let mut a = inputs.clone();
+    let mut new_moves = HashMap::default();
 
-    for round in 0..10 {
-        let mut new_moves = HashMap::default();
+    for round in 0..ROUNDS {
+        let mut count = 0;
         for elf in a.iter() {
             if let Some(new_elf) = elf.tick(&a, round) {
-                new_moves.entry(new_elf).or_insert_with(Vec::new).push(*elf);
+                new_moves
+                    .entry(new_elf)
+                    .or_insert_with(|| Vec::with_capacity(4))
+                    .push(*elf);
+                count += 1;
             }
         }
 
-        if new_moves.is_empty() {
-            break;
+        if count == 0 {
+            return ret(round, &a);
         }
 
-        for (new_pos, old_pos) in new_moves {
-            if old_pos.len() == 1 {
-                a.remove(old_pos.first().unwrap());
-                a.insert(new_pos);
-            }
-        }
-    }
-
-    let (y_min, y_max) = a.iter().map(|Elf(y, _)| *y).minmax().into_option().unwrap();
-    let (x_min, x_max) = a.iter().map(|Elf(_, x)| *x).minmax().into_option().unwrap();
-
-    (y_max.abs_diff(y_min) + 1) * (x_max.abs_diff(x_min) + 1) - (a.len() as u64)
-}
-
-#[aoc(day23, part2)]
-pub fn part2(inputs: &HashSet<Elf>) -> usize {
-    let mut a = inputs.clone();
-
-    for round in 0.. {
-        let mut new_moves = HashMap::default();
-        for elf in a.iter() {
-            if let Some(new_elf) = elf.tick(&a, round) {
-                new_moves.entry(new_elf).or_insert_with(Vec::new).push(*elf);
-            }
-        }
-
-        if new_moves.is_empty() {
-            return round + 1;
-        }
-
-        for (new_pos, old_pos) in new_moves.iter() {
+        for (new_pos, old_pos) in new_moves.iter_mut() {
             if old_pos.len() == 1 {
                 a.remove(old_pos.first().unwrap());
                 a.insert(*new_pos);
             }
+
+            old_pos.clear();
         }
     }
 
-    unimplemented!()
+    ret(ROUNDS, &a)
+}
+
+#[aoc(day23, part1)]
+pub fn part1(inputs: &HashSet<Elf>) -> u32 {
+    solve::<10, _>(inputs, |_, a| {
+        let (y_min, y_max) = a.iter().map(|Elf(y, _)| *y).minmax().into_option().unwrap();
+        let (x_min, x_max) = a.iter().map(|Elf(_, x)| *x).minmax().into_option().unwrap();
+
+        (y_max.abs_diff(y_min) + 1) * (x_max.abs_diff(x_min) + 1) - (a.len() as u32)
+    })
+}
+
+#[aoc(day23, part2)]
+pub fn part2(inputs: &HashSet<Elf>) -> usize {
+    solve::<1024, _>(inputs, |round, _| round + 1)
 }
 
 #[cfg(test)]
@@ -170,7 +164,7 @@ mod tests {
         use super::*;
 
         const INPUT: &str = include_str!("../input/2022/day23.txt");
-        const ANSWERS: (u64, usize) = (3864, 946);
+        const ANSWERS: (u32, usize) = (3864, 946);
 
         #[test]
         pub fn test() {
