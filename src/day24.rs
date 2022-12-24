9b4097bf4289d349bbc045a0a2da08154ab0b28a
Dan Nguyen <pedantic@gmail.com>
2022-12-24 02:30:50 -0600

    day24: Add walls

--- src/day24.rs
+++ src/day24.rs
@@ -10,6 +10,7 @@ const UP: u8 = 0b0001;
 const DOWN: u8 = 0b0010;
 const LEFT: u8 = 0b0100;
 const RIGHT: u8 = 0b1000;
+const WALL: u8 = 0b10000;
 
 fn square2value(sq: u8) -> Option<u8> {
     Some(match sq {
@@ -18,7 +19,7 @@ fn square2value(sq: u8) -> Option<u8> {
         b'v' => DOWN,
         b'<' => LEFT,
         b'>' => RIGHT,
-        b'#' => return None,
+        b'#' => WALL,
         _ => panic!("unknown character"),
     })
 }
@@ -32,15 +33,16 @@ fn value2square(val: u8) -> char {
         2 => 'v',
         4 => '<',
         8 => '>',
+        16 => '#',
         _ => LOOKUP[x as usize] as char,
     }
 }
 
 fn wrap_math(pos: usize, max: usize, positive: bool) -> usize {
     match positive {
-        true if pos < max - 1 => pos + 1,
-        true => 0,
-        false if pos == 0 => max - 1,
+        true if pos < max - 2 => pos + 1,
+        true => 1,
+        false if pos == 1 => max - 2,
         false => pos - 1,
     }
 }
@@ -77,19 +79,24 @@ impl World {
                     continue;
                 }
 
-                if cell & UP > 0 {
+                if cell == &WALL {
+                    world[ridx][cidx] = WALL;
+                    continue;
+                }
+
+                if cell & UP == UP {
                     world[wrap_math(ridx, height, false)][cidx] |= UP;
                 }
 
-                if cell & DOWN > 0 {
+                if cell & DOWN == DOWN {
                     world[wrap_math(ridx, height, true)][cidx] |= DOWN;
                 }
 
-                if cell & LEFT > 0 {
+                if cell & LEFT == LEFT {
                     world[ridx][wrap_math(cidx, width, false)] |= LEFT;
                 }
 
-                if cell & RIGHT > 0 {
+                if cell & RIGHT == RIGHT {
                     world[ridx][wrap_math(cidx, width, true)] |= RIGHT;
                 }
             }
@@ -101,19 +108,16 @@ impl World {
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
 struct State {
-    pos: Option<(usize, usize)>,
+    pos: (usize, usize),
     world_num: usize,
 }
 
 #[aoc_generator(day24)]
 pub fn generator(input: &str) -> World {
-    let mut lines = input.lines();
-    lines.next();
-
     let mut res = Vec::new();
 
-    for line in lines {
-        if !line.starts_with("##") {
+    for line in input.lines() {
+        if !line.is_empty() {
             res.push(line.bytes().filter_map(square2value).collect())
         }
     }
@@ -129,44 +133,41 @@ fn get_world(n: usize, worlds: &mut Vec<World>) -> &World {
     worlds.get(n).unwrap()
 }
 
+fn solve(
+    worlds: &mut Vec<World>,
+    world_num: usize,
+    start: (usize, usize),
+    end: (usize, usize),
+) -> usize {
+    let height = worlds[0].world.len();
+    let width = worlds[0].world[0].len();
+    let res = bfs(
+        &State {
+            pos: start,
+            world_num,
+        },
+        |state| {
+            let world_num = state.world_num + 1;
+            let (r, c) = state.pos;
+            std::iter::once((r, c))
+                .chain(neighbors(r, c, height, width))
+                .filter(|&(y, x)| get_world(world_num, worlds).world[y][x] == NONE)
+                .map(|pos| State { pos, world_num })
+                .collect::<Vec<_>>()
+        },
+        |state| state.pos == (end.0, end.1),
+    );
+
+    world_num + res.unwrap().len() - 1
+}
+
 #[aoc(day24, part1)]
 pub fn part1(inputs: &World) -> usize {
     let height = inputs.world.len();
     let width = inputs.world[0].len();
     let mut worlds = vec![inputs.clone()];
 
-    let res = bfs(
-        &State {
-            pos: None,
-            world_num: 0,
-        },
-        |state| {
-            let world_num = state.world_num + 1;
-            if let Some((r, c)) = state.pos {
-                std::iter::once((r, c))
-                    .chain(neighbors(r, c, height, width))
-                    .filter(|&(y, x)| get_world(world_num, &mut worlds).world[y][x] == NONE)
-                    .map(|pos| State {
-                        pos: Some(pos),
-                        world_num,
-                    })
-                    .collect()
-            } else if get_world(world_num, &mut worlds).world[0][0] == NONE {
-                vec![State {
-                    pos: Some((0, 0)),
-                    world_num,
-                }]
-            } else {
-                vec![State {
-                    pos: state.pos,
-                    world_num,
-                }]
-            }
-        },
-        |state| state.pos == Some((height - 1, width - 1)),
-    );
-
-    res.unwrap().len()
+    solve(&mut worlds, 0, (0, 1), (height - 1, width - 2))
 }
 
 #[aoc(day24, part2)]
@@ -175,104 +176,9 @@ pub fn part2(inputs: &World) -> usize {
     let width = inputs.world[0].len();
     let mut worlds = vec![inputs.clone()];
 
-    let res = bfs(
-        &State {
-            pos: None,
-            world_num: 0,
-        },
-        |state| {
-            let world_num = state.world_num + 1;
-            if let Some((r, c)) = state.pos {
-                std::iter::once((r, c))
-                    .chain(neighbors(r, c, height, width))
-                    .filter(|&(y, x)| get_world(world_num, &mut worlds).world[y][x] == NONE)
-                    .map(|pos| State {
-                        pos: Some(pos),
-                        world_num,
-                    })
-                    .collect()
-            } else if get_world(world_num, &mut worlds).world[0][0] == NONE {
-                vec![State {
-                    pos: Some((0, 0)),
-                    world_num,
-                }]
-            } else {
-                vec![State {
-                    pos: state.pos,
-                    world_num,
-                }]
-            }
-        },
-        |state| state.pos == Some((height - 1, width - 1)),
-    );
-
-    let mut move_num = res.unwrap().len() + 1;
-
-    let res = bfs(
-        &State {
-            pos: None,
-            world_num: move_num,
-        },
-        |state| {
-            let world_num = state.world_num + 1;
-            if let Some((r, c)) = state.pos {
-                std::iter::once((r, c))
-                    .chain(neighbors(r, c, height, width))
-                    .filter(|&(y, x)| get_world(world_num, &mut worlds).world[y][x] == NONE)
-                    .map(|pos| State {
-                        pos: Some(pos),
-                        world_num,
-                    })
-                    .collect()
-            } else if get_world(world_num, &mut worlds).world[height - 1][width - 1] == NONE {
-                vec![State {
-                    pos: Some((height - 1, width - 1)),
-                    world_num,
-                }]
-            } else {
-                vec![State {
-                    pos: state.pos,
-                    world_num,
-                }]
-            }
-        },
-        |state| state.pos == Some((0, 0)),
-    );
-
-    move_num += res.unwrap().len() + 1;
-
-    let res = bfs(
-        &State {
-            pos: None,
-            world_num: move_num,
-        },
-        |state| {
-            let world_num = state.world_num + 1;
-            if let Some((r, c)) = state.pos {
-                std::iter::once((r, c))
-                    .chain(neighbors(r, c, height, width))
-                    .filter(|&(y, x)| get_world(world_num, &mut worlds).world[y][x] == NONE)
-                    .map(|pos| State {
-                        pos: Some(pos),
-                        world_num,
-                    })
-                    .collect()
-            } else if get_world(world_num, &mut worlds).world[0][0] == NONE {
-                vec![State {
-                    pos: Some((0, 0)),
-                    world_num,
-                }]
-            } else {
-                vec![State {
-                    pos: state.pos,
-                    world_num,
-                }]
-            }
-        },
-        |state| state.pos == Some((height - 1, width - 1)),
-    );
-
-    move_num + res.unwrap().len()
+    let move_num = solve(&mut worlds, 0, (0, 1), (height - 1, width - 2));
+    let move_num = solve(&mut worlds, move_num, (height - 1, width - 2), (0, 1));
+    solve(&mut worlds, move_num, (0, 1), (height - 1, width - 2))
 }
 
 #[cfg(test)]
@@ -300,7 +206,7 @@ mod tests {
 
     #[test]
     pub fn test2() {
-        // assert_eq!(part2(&generator(SAMPLE)), 54);
+        assert_eq!(part2(&generator(SAMPLE)), 54);
     }
 
     mod regression {
