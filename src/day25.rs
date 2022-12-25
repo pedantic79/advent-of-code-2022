85d88ba95010fb5e8aea50b734912238ef3382de
Dan Nguyen <pedantic@gmail.com>
2022-12-25 01:58:05 -0600

    day25: Add more tests

--- src/day25.rs
+++ src/day25.rs
@@ -76,26 +76,29 @@ mod tests {
 
     #[test]
     pub fn from_snafu() {
-        const A: [(i64, &[u8]); 15] = [
-            (1, b"1"),
-            (2, b"2"),
-            (3, b"1="),
-            (4, b"1-"),
-            (5, b"10"),
-            (6, b"11"),
-            (7, b"12"),
-            (8, b"2="),
-            (9, b"2-"),
-            (10, b"20"),
-            (15, b"1=0"),
-            (20, b"1-0"),
+        const A: [(i64, &[u8]); 4] = [
             (2022, b"1=11-2"),
             (12345, b"1-0---0"),
             (314159265, b"1121-1110-1=0"),
+            (27182818284, b"1-212-==002-112-"),
         ];
 
         for (i, s) in A {
             assert_eq!(snafu2decimal(s), i)
+        }
+    }
+
+    #[test]
+    pub fn count_snafu() {
+        const A: &[&[u8]] = &[
+            b"0", b"1", b"2", b"1=", b"1-", b"10", b"11", b"12", b"2=", b"2-", b"20", b"21", b"22",
+            b"1==", b"1=-", b"1=0", b"1=1", b"1=2", b"1-=", b"1--", b"1-0", b"1-1", b"1-2", b"10=",
+            b"10-", b"100", b"101", b"102", b"11=", b"11-", b"110", b"111", b"112", b"12=", b"12-",
+            b"120", b"121", b"122", b"2==", b"2=-", b"2=0", b"2=1", b"2=2", b"2-=", b"2--", b"2-0",
+        ];
+
+        for (i, s) in A.iter().enumerate() {
+            assert_eq!(snafu2decimal(s), i as i64)
         }
     }
 
@@ -107,6 +110,7 @@ mod tests {
     #[test]
     pub fn test1() {
         assert_eq!(part1(&generator(SAMPLE)), "2=-1=0");
+        println!("{}", part1(&9890));
     }
 
     mod regression {
@@ -120,6 +124,7 @@ mod tests {
             let input = INPUT.trim_end_matches('\n');
             let output = generator(input);
 
+            assert_eq!(output, 37067035390042);
             assert_eq!(part1(&output), ANSWERS);
         }
     }
